a011d67a592279dca6a1a0a06bedf106caf2359e
Abhi181998 <91414271+Abhi181998@users.noreply.github.com>
2024-05-29 07:50:26 +0000

    variable constant

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,11 @@
+ const accountId=14453
+ let accountEmail="abhiarya1618@gmail.com"
+ var accountPassword="12345"
+ accountcity="bengaluru"
+let accountstate; /*undefined variable */
+ console.log(accountId);//for single //
+ accountEmail="jdwkec@gmial.com"
+ accountPassword="68723"
+ accountcity="jaipur"
+ console.table([accountEmail,accountstate,accountId,accountPassword,accountcity])//for multiple at once//
+ /*prefer not to use var because of issue in block and functional scope*/
