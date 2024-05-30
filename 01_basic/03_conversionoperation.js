0dc0bd2899280249406f4976a20f98b457144ebb
Abhi181998 <91414271+Abhi181998@users.noreply.github.com>
2024-05-30 06:47:21 +0000

    conversion of data types

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,19 @@
+//let score ="33abc", true;
+
+//console.log(typeof score);
+//console.log(typeof (score));
+//let valueinnumber=Number(score)
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber);//nan means not a number
+
+
+                      //"33"=33
+                      //"33abc"= nan
+                      //true=1
+let loggedin= 1
+let booleanisloggedin = Boolean(loggedin)
+console.log(booleanisloggedin);  // value will be true
+let someumber=55
+let stringnumber =String(someumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
