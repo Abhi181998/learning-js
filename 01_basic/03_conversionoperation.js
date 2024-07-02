1ea098dcd94b5589a5e70b04b1dd26731adbd097
Abhi181998 <91414271+Abhi181998@users.noreply.github.com>
2024-07-02 09:57:31 +0000

    ys

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -10,10 +10,36 @@
                       //"33"=33
                       //"33abc"= nan
                       //true=1
-let loggedin= 1
-let booleanisloggedin = Boolean(loggedin)
-console.log(booleanisloggedin);  // value will be true
-let someumber=55
-let stringnumber =String(someumber)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// let loggedin= 1
+// let booleanisloggedin = Boolean(loggedin)
+// console.log(booleanisloggedin);  // value will be true
+// let someumber=55
+// let stringnumber =String(someumber)
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+//********************************* operations******************************8//
+
+
+// let value =3
+// let negvalue = -value
+// console.log(negvalue)
+// console.log(2+2)
+
+// console.log(2**2)
+// console.log(2/2)
+
+// adding two string 
+// let str1 = "hello"
+// let str2 = "abhishek "
+// let str3 = str1+str2
+// console.log(str3)
+console.log(+true);
+console.log(+"");
+// https://tc39.es/ecma262/#sec-abstract-operations ----for study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+// postfix and prefix
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+// if used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
