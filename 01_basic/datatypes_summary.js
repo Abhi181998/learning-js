434f9afac42ba0b9f3d415d0be37150f42850303
Abhi181998 <91414271+Abhi181998@users.noreply.github.com>
2024-07-04 03:13:59 +0000

    data types

---
+++ 01_basic/datatypes_summary.js
@@ -0,0 +1,46 @@
+//*******************premitive datatypes***************************//
+// const score = 100;
+// const isloggedin = false
+// const outsidetemp = null
+// let userEmailid;
+// const id = Symbol('123')
+// const bigNumber = 1234567n
+
+// console.log(typeof score)
+// console.log(typeof isloggedin)
+// console.log(typeof outsidetemp)
+// console.log(typeof userEmailid)
+// console.log(typeof id)
+// console.log(typeof bigNumber)
+
+// @Abhi181998 ➜ /workspaces/learning-js (main) $ node 01_basic/datatypes_summary.js
+// number
+// boolean
+// object
+// undefined
+// symbol
+// bigint
+
+
+
+//*********************refrencetypes or non premitive ****************/
+//array
+// const  Heros =["luffy","zoro","sanzi","sabo"]
+// //object 
+// let myobj = {
+//     name:"abhishek",
+//     age: 25,
+// }
+// // function 
+// const myFunction=function(){
+//     console.log("hello world");
+// }
+// console.log(typeof Heros);
+// console.log(typeof myobj);
+// console.log(typeof myFunction);
+
+// @Abhi181998 ➜ /workspaces/learning-js (main) $ node 01_basic/datatypes_summary.js
+// object
+// object
+// function
+//https://262.ecma-international.org/5.1/#sec-11.4.3
